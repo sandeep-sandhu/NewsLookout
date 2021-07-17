f3677767ffc194041b15a4d424ebf89fa5669cdc
Sandeep Singh Sandhu <sandeep.sandhu@gmx.com>
2021-07-17 22:20:22 +0530

    Version 2.0.0
    Updated to version 2.0.0, includes several bug-fixes and refactoring related changes.

---
+++ newslookout.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+rem Run the scraper application
+rem Pass the first argument
+
+cd /d "%~dp0"
+
+if [%1]==[] goto makedate
+
+python src\scraper_app.py -c conf\newslookout_win.conf -d %1
+
+goto end
+
+:makedate
+
+rem set rundate to today's date as YYYY-MM-DD
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+
+set today=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
+
+python src\scraper_app.py -c conf\newslookout_win.conf -d %today%
+
+:end
+echo Program completed successfully.
